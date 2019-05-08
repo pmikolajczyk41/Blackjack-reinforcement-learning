7efd8a90e1e23ce3c19e15255816fa2ffc03eade
pmikolajczyk41 <piotrekmiko.24@onet.eu>
2019-05-08 22:31:01 +0200

    Plot probabilistic policy

--- model/policy.py
+++ model/policy.py
@@ -8,11 +8,19 @@ class Policy:
     def make_decision_in(self, state: State) -> Action:
         raise NotImplemented
 
+    def stick_certainty_in(self, state: State) -> float:
+        raise NotImplemented
+
+    def hit_certainty_in(self, state: State) -> float:
+        return 1. - self.stick_certainty_in(state)
+
     @classmethod
     def from_deterministic_mapping(cls, mapping: dict):
         policy = Policy()
         setattr(policy, 'make_decision_in',
                 lambda state: mapping[state])
+        setattr(policy, 'stick_certainty_in',
+                lambda state: 0. if mapping[state] == Action.HIT else 1.)
         return policy
 
     @classmethod
@@ -20,4 +28,6 @@ class Policy:
         policy = Policy()
         setattr(policy, 'make_decision_in',
                 lambda state: np.random.choice(list(Action), p=mapping[state]))
+        setattr(policy, 'stick_certainty_in',
+                lambda state: mapping[state][Action.STICK.value])
         return policy
