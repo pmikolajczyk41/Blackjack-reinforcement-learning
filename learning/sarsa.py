678ac4012273e4b5ce065edf00ca6a17a87f71fc
pmikolajczyk41 <piotrekmiko.24@onet.eu>
2019-05-10 10:45:48 +0200

    Sarsa implemented

--- learning/sarsa.py
+++ learning/sarsa.py
@@ -1,4 +1,5 @@
-from learning.learning_utils import Algorithm
+from learning.learning_utils import Algorithm, StateActionPair
+from model.game import Game
 from model.policy import Policy
 
 
@@ -18,4 +19,16 @@ class Sarsa(Algorithm):
             return Policy.epsilon_greedy_from_values(self._Q, lambda: 1. / denominator)
 
     def train(self, rounds: int) -> None:
-        super().train(rounds)
+        for i in range(rounds):
+            game_info = Game(player_policy=self.policy).play()
+            # can do offline due to the acyclic game
+            self._update_with(game_info)
+
+    def _update_with(self, game_info):
+        for (log, next_log) in zip(game_info.player_logs[:-1], game_info.player_logs[1:]):
+            sap = StateActionPair(*log)
+            next_sap = StateActionPair(*next_log)
+            self._Q[sap] += self._alpha * (self._gamma * self._Q[next_sap] - self._Q[sap])
+
+        last_sap = StateActionPair(*(game_info.player_logs[-1]))
+        self._Q[last_sap] += self._alpha * (game_info.winner - self._Q[last_sap])
